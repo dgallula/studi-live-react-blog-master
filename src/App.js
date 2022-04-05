8c5f3c1d8a34d4b92070fd55e7677d78fba7d646
Your Name <you@example.com>
2022-04-05 10:09:14 +0300

    projet react tuto init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Meal from "./Meal.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Bonsoir aux étudiants Studi en ce mardi</p>
+      </header>
+      <Meal />
+    </div>
+  );
+}
+
+export default App;
